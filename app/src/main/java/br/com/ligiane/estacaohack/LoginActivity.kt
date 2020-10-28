564c76d3304ca04779fa90f9b24c6c9604b65d20
Ligiane <ligiane.souza@live.com>
2020-10-28 17:22:47 -0300

    update Login_Activity

--- app/src/main/java/br/com/ligiane/estacaohack/LoginActivity.kt
+++ app/src/main/java/br/com/ligiane/estacaohack/LoginActivity.kt
@@ -1,11 +1,72 @@
 package br.com.ligiane.estacaohack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Escutar o clique do botão entrar
+        btnLoginEntrar.setOnClickListener {
+
+            // Capturar os dados digitados
+            // atributo text guarda texto crú RawText (String "...")
+            // conversão de tipo (Casting)
+            val email = edtLoginEmail.text.toString().trim().toLowerCase()
+            val senha = edtLoginSenha.text.toString().trim()
+
+            // Validação dos campos
+            if (email.isEmpty()) {
+                edtLoginEmail.error = "Campo obrigatório"
+                edtLoginEmail.requestFocus()
+
+            } else if (senha.isEmpty()) {
+                edtLoginSenha.error = "Campo obrigatório"
+                edtLoginSenha.requestFocus()
+
+            } else {
+
+                // Recuperar os dados do cadastro (SharedPreferences)
+
+                // Simulando o acesso aos dados persistidos
+                // Viricar o email e senha
+                if (email == "Ligi@cellep.com" && senha == "1234") {
+
+                    // Exibir uma mensagem ao usuário
+                    // contexto
+                    // mensagem
+                    // duração do Toast
+                    Toast.makeText(this, "Usuário logado", Toast.LENGTH_LONG).show()
+
+                    // Abrir a MainActivity
+                    val mIntent = Intent(this, MainActivity::class.java)
+                    startActivity(mIntent)
+                    finish()
+
+                } else {
+
+                    // Apresentar mensagem de erro ao usuário
+                    Toast.makeText(this, "Email ou senha inválidos", Toast.LENGTH_LONG).show()
+
+                }
+
+            }
+
+        }
+
+        // Escutar o clique do botão cadastrar
+        btnLoginCadastrar.setOnClickListener {
+            val mIntent = Intent(this, CadastroActivity::class.java)
+            startActivity(mIntent)
+        }
+
     }
 }
--- app/src/main/java/br/com/ligiane/estacaohack/MainActivity.kt
+++ app/src/main/java/br/com/ligiane/estacaohack/MainActivity.kt
@@ -1,11 +1,72 @@
 package br.com.ligiane.estacaohack
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recuperar o email passado através da Intent
+        val email = intent.getStringExtra("INTENT_EMAIL")
+
+        // Acessar o arquivo de preferencias compartilhadas
+        val sharedPrefs = getSharedPreferences("cadastro_$email", Context.MODE_PRIVATE)
+
+        // Recuperar os dados no arquivo de preferências compartilhadas
+        val nome = sharedPrefs.getString("NOME", "")
+        val sobrenome = sharedPrefs.getString("SOBRENOME", "")
+        val genero = sharedPrefs.getString("GENERO", "")
+
+        // Exibir os dados recuperados e testar o App
+        txvMainNome.text = "$nome $sobrenome"
+        txvMainEmail.text = email
+        txvMainGenero.text = genero
+
+        // Botão site Cel.Lep
+        btnMainSite.setOnClickListener {
+            val mIntent = Intent(this, WebActivity::class.java)
+            startActivity(mIntent)
+        }
+
+        // Botão sair
+        btnMainSair.setOnClickListener {
+
+            // Criar a caixa de dialogo
+            val alert = AlertDialog.Builder(this)
+
+            // Definir o título
+            alert.setTitle("Atenção")
+
+            // Definir a mensagem
+            alert.setMessage("Deseja mesmo sair?")
+
+            // Definir o rótudo e as ações do botão positivo
+            alert.setPositiveButton("Sair") { dialog, which ->
+
+                val mIntent = Intent(this, LoginActivity::class.java)
+                startActivity(mIntent)
+                finishAffinity()
+
+            }
+
+            // Definir o rótulo e as ações do botão neutro
+            alert.setNeutralButton("Não") { dialog, which -> }
+
+            // Definir se a caixa é cancelável
+            alert.setCancelable(false)
+
+            // Exibir a caixa de dialogo
+            alert.show()
+
+        }
+
     }
+
 }
